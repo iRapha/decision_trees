59aafeedc2cde539c8198e26ac966e7716f6f929
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-01-13 15:09:10 -0500

    add sample dataset

---
+++ sample_dataset.py
@@ -0,0 +1,9 @@
+sample_dataset = [
+  ({'F1': True, 'F2': True, 'F3': False, 'F4': False, 'F5': False}, 1),
+  ({'F1': False, 'F2': False, 'F3': True, 'F4': True, 'F5': False}, 1),
+  ({'F1': True, 'F2': False, 'F3': False, 'F4': True, 'F5': False}, 1),
+  ({'F1': True, 'F2': False, 'F3': True, 'F4': False, 'F5': True}, 1),
+  ({'F1': False, 'F2': True, 'F3': False, 'F4': False, 'F5': False}, 0),
+  ({'F1': True, 'F2': True, 'F3': False, 'F4': True, 'F5': True}, 0),
+  ({'F1': False, 'F2': True, 'F3': True, 'F4': True, 'F5': True}, 0)
+]
